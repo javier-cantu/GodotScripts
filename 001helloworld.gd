8fe1852eb3486f604993859bd8378db48b89ba31
Javier <pacocantu87@gmail.com>
2021-11-11 10:47:56 -0600

    Create 001helloworld.gd

---
+++ 001helloworld.gd
@@ -0,0 +1,9 @@
+# Este script si se attach a un nodo
+# hará que se envíe el string "Hello World" al Output en Godot
+# Vide de Youtube donde lo explico: https://youtu.be/nknqWofTZNo
+
+extends Node
+
+func _ready():
+  # Queremos mandar un mensaje al output
+  print("Hello World")
